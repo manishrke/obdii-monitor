a5f6eda63945f9ef610edba9546efce6132450ad
BSchoch <BSchoch@981f318b-7b51-8592-1405-d8a2172e8407>
2010-10-30 22:34:52 +0000

    Adding GPS classes that I forgot to add in the last commit.

---
+++ obdii-monitor/ObdiiMonitor/GPS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ObdiiMonitor
+{
+    class GPS
+    {
+        private Controller controller;
+
+        public Controller Controller
+        {
+            set { controller = value; }
+        }
+
+        private ArrayList gpsList = new ArrayList();
+
+        public ArrayList GpsList
+        {
+            get { return gpsList; }
+        }
+
+        public GPSCoordinate get(uint time)
+        {
+            int length = gpsList.Count;
+
+            for (int i = 0; i < length; ++i)
+            {
+                GPSCoordinate coord = (GPSCoordinate)gpsList[i];
+                if (coord.Time < time)
+                    continue;
+
+                return coord;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ obdii-monitor/ObdiiMonitor/GPSCoordinate.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObdiiMonitor
+{
+    class GPSCoordinate
+    {
+        private string horizontal;
+
+        public string Horizontal
+        {
+            get { return horizontal; }
+        }
+
+        private string vertical;
+
+        public string Vertical
+        {
+            get { return vertical; }
+        }
+
+        private uint time;
+
+        public uint Time
+        {
+            get { return time; }
+        }
+
+        public GPSCoordinate(uint time, string horizontal, string vertical)
+        {
+            this.time = time;
+            this.horizontal = horizontal;
+            this.vertical = vertical;
+        }
+
+        public GPSCoordinate(uint time, string coordinates)
+        {
+            int index = 0;
+
+            while ((index < coordinates.Length) && (coordinates[index] != 'N') && (coordinates[index] != 'S'))
+            {
+                ++index;
+            }
+
+            if (index >= coordinates.Length)
+            {
+                return;
+            }
+
+            ++index;
+
+            vertical = coordinates.Substring(0, index);
+
+            int index2 = index;
+
+            while ((index2 < coordinates.Length) && (coordinates[index2] != 'E') && (coordinates[index2] != 'W'))
+            {
+                ++index2;
+            }
+
+            if (index2 >= coordinates.Length)
+            {
+                return;
+            }
+
+            ++index2;
+
+            horizontal = coordinates.Substring(index, index2 - index);
+
+        this.time = time;
+        }
+
+        public GPSCoordinate(PollResponse response) : this (response.Time, response.Data)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return horizontal + " " + vertical;
+        }
+    }
+}
